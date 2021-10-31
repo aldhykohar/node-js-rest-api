b5a55b42e5f004de3aadb301492a05a85fe6ba53
aldhykohar <aldhykohar@stimednp.ac.id>
2021-10-31 17:25:38 +0800

    :sparkles: add file connection

---
+++ connection.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+
+//create connection
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodejs_api'
+});
+
+conn.connect((err) => {
+    if (err) throw err;
+    console.log('Mysql connected');
+});
+
+module.exports = conn;
