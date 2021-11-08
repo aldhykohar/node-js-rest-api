74ddff9f81aa74a53958345db8ee8b44e9acd84d
aldhykohar <aldhykohar@stimednp.ac.id>
2021-11-08 21:12:08 +0800

    :sparkles: add validate model

--- config/model/mahasiswa.js
+++ config/model/mahasiswa.js
@@ -5,11 +5,25 @@ var mahasiswa = db.define('mahasiswa', {
     id_mahasiswa: Sequelize.INTEGER,
     nim: {
         type: Sequelize.DataTypes.STRING,
-        notEmpty: true,
-        notNull: true
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
     },
-    nama: Sequelize.STRING,
-    jurusan: Sequelize.STRING,
+    nama: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    jurusan: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
 }, {
     freezeTableName: true,
     timestamps: false
