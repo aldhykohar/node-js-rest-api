1226b447f42e0d9c9130066131a15b39bea738fd
aldhykohar <aldhykohar@stimednp.ac.id>
2021-11-27 11:47:23 +0800

    :sparkles: create user model

---
+++ config/model/user.js
@@ -0,0 +1,47 @@
+const Sequelize = require('sequelize')
+const db = require('../database/connection')
+
+var mahasiswa = db.define('mahasiswa', {
+    id_user: Sequelize.INTEGER,
+    username: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    email: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    password: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    role: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    tgl_regis: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+}, {
+    freezeTableName: true,
+    timestamps: false
+})
+
+mahasiswa.removeAttribute('id')
+module.exports = mahasiswa
